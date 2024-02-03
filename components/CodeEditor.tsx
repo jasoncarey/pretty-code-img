a71b7e7a641779d01bdee48ff3dd070f91b2a2d5
Jason Carey <jasdcarey@gmail.com>
2024-02-03 14:13:46 +1100

    feat: add drag handles

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -69,6 +69,10 @@ function CodeEditor({
       style={{ background: background }}
     >
       <div className='code-block' style={{ padding: currentPadding }}>
+        <div className='handle handle-top absolute left-1/2 translate-x-[-50%] top-[-4px] w-2 h-2 rounded-full bg-slate-300 hover:bg-slate-50'></div>
+        <div className='handle handle-bottom absolute left-1/2 translate-x-[-50%] bottom-[-4px] w-2 h-2 rounded-full bg-slate-300 hover:bg-slate-50'></div>
+        <div className='handle handle-left absolute left-[-4px] top-1/2 w-2 h-2 rounded-full bg-slate-300 hover:bg-slate-50'></div>
+        <div className='handle handle-right absolute right-[-4px] top-1/2 w-2 h-2 rounded-full bg-slate-300 hover:bg-slate-50'></div>
         <div className='code-title h-[52px] px-4 flex items-center justify-between bg-black bg-opacity-80'>
           <div className='dots flex items-center gap-2'>
             <div className='w-3 h-3 rounded-full bg-[#ff5656]'></div>
